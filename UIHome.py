7ecf31c62bf778bba3fc1133638635b4719f001e
dongtlt <tranthangdong@gmail.com>
2019-07-10 09:19:33 -0500

    Thêm chức năng lưu số trong list, phát âm thanh mới hiển thị số trên UI

--- UIHome.py
+++ UIHome.py
@@ -311,7 +311,6 @@ def qmsList():
             w.setQlbelPreviousColor()
             w.setQlbelColor(Gate)
             ## Play Sound
-
             soundProcess(str(Number), str(Gate))
         time.sleep(0.5) # wait
 
